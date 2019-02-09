83f159898d7238131729ab4d2137c2c297e770c2
Andrew McKay <andrew.s.mckay@gmail.com>
2019-02-08 22:49:15 -0800

    laid groundwork for adding, moving, and removing annotations

--- src/imgtrainerfxns.jl
+++ src/imgtrainerfxns.jl
@@ -1,3 +1,45 @@
+function add_remove_add!(scene, points, pplot,clicks)
+    on(events(scene).mousebuttons) do but
+        if ispressed(but, Mouse.left) && ispressed(scene, Keyboard.left_control)
+            pos = to_world(scene, Point2f0(events(scene).mouseposition[]))
+            push!(points[], pos)
+            points[] = points[]
+        elseif ispressed(but, Mouse.right)
+            plot, idx = Makie.mouse_selection(scene)
+            if plot == pplot && checkbounds(Bool, points[], idx)
+                deleteat!(points[], idx)
+                points[] = points[]
+            end
+        elseif ispressed(but, Mouse.left)
+            pos = to_world(scene, Point2f0(events(scene).mouseposition[]))
+            push!(clicks, push!(clicks[], pos))
+         end
+    end
+    return
+end
+
+function add_move!(scene, points, pplot)
+    idx = Ref(0); dragstart = Ref(false); startpos = Base.RefValue(Point2f0(0))
+    on(events(scene).mousedrag) do drag
+        if ispressed(scene, Mouse.left)
+            if drag == Mouse.down
+                plot, _idx = Makie.mouse_selection(scene)
+                if plot == pplot
+                    idx[] = _idx; dragstart[] = true
+                    startpos[] = to_world(scene, Point2f0(scene.events.mouseposition[]))
+                end
+            elseif drag == Mouse.pressed && dragstart[] && checkbounds(Bool, points[], idx[])
+                pos = to_world(scene, Point2f0(scene.events.mouseposition[]))
+                points[][idx[]] = pos
+                points[] = points[]
+            end
+        else
+            dragstart[] = false
+        end
+        return
+    end
+end
+
 function imagetrain(imgpath, pathout)
     # first, load our image
     # imgpath = "/Users/am/Dropbox/imagetrainertest/test.tif"
@@ -6,24 +48,16 @@ function imagetrain(imgpath, pathout)
 
     # then, setup environment
     # http://makie.juliaplots.org/stable/examples-interaction.html
-    #scene = Scene()#raw = true, camera = campixel!, resolution = (2500,1300))
     i1 = image(img);
-
+    points = Node(Point2f0[])
     clicks = Node(Point2f0[(0,0)])
-    on(i1.events.mousebuttons) do buttons
-        if ispressed(i1, Mouse.left)
-            pos = to_world(i1, Point2f0(i1.events.mouseposition[]))
-            println(clicks)
-            push!(clicks, push!(clicks[], pos))
-        end
-        return
-     end
+    
+    pplot = i1
 
-    #t = Theme(raw = true, camera = campixel!, resolution = (2500,1300))
+    points[] = points[]
+
     nextimg= button(raw = true, camera = campixel!, "Next Image")
     saveout = button(raw = true, camera = campixel!, "Save Annotations")
-
-
     on(nextimg[end][:clicks]) do c
         println("")
         println("clicked Next Image")
@@ -32,10 +66,14 @@ function imagetrain(imgpath, pathout)
 
     on(saveout[end][:clicks]) do c
         println("saving to $pathout")
-         writedlm(pathout, clicks.val, ',')
-         #p2[:marker] = markers[rand(1:5)]
+        println(points.val)
+        println(length(points.val))
+         #writedlm(pathout, points.val, ',')
     end
 
-    scatter!(i1, clicks, color = :red, marker = '+', markersize = 50)
-    RecordEvents(hbox(i1, vbox(nextimg, saveout),  parent = Scene(resolution = (500,500))), pathout)
+    add_move!(i1, points, pplot)
+    add_remove_add!(i1, points, pplot, clicks)
+    scatter!(i1, points, color = :red, marker = '+', markersize = 50)
+    # left command + left mouse button adds, right mouse button removes, left mouse button alone moves
+    RecordEvents(hbox(i1, vbox(nextimg, saveout),  parent = Scene(resolution = (1000,1000))), pathout)
 end
